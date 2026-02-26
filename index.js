0e79d57998c9b837275cca4d2b5bbe41e351ba04
nairx <nairxprv@gmail.com>
2026-02-26 11:16:59 +0530

    updated

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
 app.set("view engine", "ejs");
-app.listen(8080);
-
-const products = [
-  { id: 1, name: "Product 1", price: 34 },
-  { id: 2, name: "Product 2", price: 30 },
-  { id: 3, name: "Product 3", price: 50 },
-];
-
-app.get("/", (req, res) => {
-  // res.json({ message: "Hello World" });
-  res.render("index", { name: "John" });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+const dbConnect = async () => {
+  await mongoose.connect("mongodb://localhost:27017/merndatabase");
+};
+const startServer = async () => {
+  await dbConnect();
+  app.listen(8080, () => console.log("Server started"));
+};
+const productSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  imageurl: { type: String, required: true },
 });
-
-app.get("/products", (req, res) => {
-res.render("products", {products});
+const productModel = mongoose.model("products", productSchema);
+app.get("/", async (req, res) => {
+  const products = await productModel.find();
+  res.json(products);
 });
+app.post("/", async (req, res) => {
+  const body = req.body;
+  const result = await productModel.create(body);
+  res.json({ message: "Product created" });
+});
+startServer();
